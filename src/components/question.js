c687b8c7cb7547a5e9e9f4d6499a2d68d7878f0c
ptlergo <ptlergo@gmail.com>
2016-10-22 23:25:15 -0400

    listview added to scene

--- src/components/question.js
+++ src/components/question.js
@@ -4,16 +4,16 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
-  TextInput
+  ListView
 } from 'react-native'
 
 export default class Question extends Component {
-
-  constructor(props){
-    super(props)
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-    textdata: ''
-    }
+      dataSource: ds.cloneWithRows(['100', '200', 'Spaghetti']),
+    };
   }
   onButtonPress(){
     this.props.navigator.push({
@@ -24,7 +24,10 @@ export default class Question extends Component {
   render(){
     return(
       <View style={styles.container}>
-      <Text style={styles.questiontext}>What Is The Length Of A Football Field?</Text>
+        <Text style={styles.questiontext}>What Is The Length Of A Football Field?</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text style={styles.choice}>{rowData}</Text>} />
       </View>
     )
   }
@@ -34,6 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    padding: 50,
     alignItems: 'center',
     backgroundColor: '#f5fcff'
   },
